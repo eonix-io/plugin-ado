59fa636c7ad5861fc3777e33ba1e64f6224c1b4a
Josh Kraker <joshkrak@gmail.com>
2021-02-23 20:17:52 -0500

    Initial setup

---
+++ src/components/AdoConfig.vue
@@ -0,0 +1,18 @@
+<template>
+   This is the config component
+</template>
+
+<script lang="ts">
+   import { defineComponent } from 'vue';
+
+
+   export default defineComponent({
+      setup() {
+         console.log('In setup');
+      }
+   });
+
+</script>
+
+<style>
+</style>
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+
+export { default as AdoConfig } from './components/AdoConfig.vue';
